4efee1e8b826feeca269eca0826c95b4ac56be1a
kenpcjr <kennethcooke@me.com>
2016-04-29 14:05:46 -0400

    Coyote Smashed.

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -5,11 +5,21 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    /**
-     
-     * Write your loop here!
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(25) + 26;
     
-     */
+    do {
+        if (steps % 10 == 0 && steps != 0) {
+            NSLog(@"YOU'RE A CUCKOO!");
+        }
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        steps++;
+        NSLog(@"Meep! Meep!");
+        NSLog(@"%lu",steps);
+    } while (steps <= anvil);
+    
     
     // do not alter
     return YES;  //
